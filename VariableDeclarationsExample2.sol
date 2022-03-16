93bc7f7b778064d9ca961dbfd96729f16639984f
K1-R1 <kyranrawlinson@gmail.com>
2022-03-16 18:16:30 +0000

    Added VariableDeclarationsExample2.sol

---
+++ VariableDeclarationsExample2.sol
@@ -0,0 +1,10 @@
+/*
+    If the declared variable should have a type different from the default type, you denote that following a colon. 
+    You can also declare multiple variables in one statement when you assign from a function call that returns multiple values.
+*/
+
+{
+    let zero:u32 := 0:u32
+    let v:u256, t:u32 := f()
+    let x, y := g()
+}
